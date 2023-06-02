8b6308bad0a01fb567ae3cede0687caa71b2847e
mcti2dcoronado <126918861+mcti2dcoronado@users.noreply.github.com>
2023-06-02 19:02:01 -0400

    Update main.tf
    Remove Terraform block

--- main.tf
+++ main.tf
@@ -1,24 +1,4 @@
-# Terraform Block
 
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-    null = {
-      source = "hashicorp/null"
-      version = "~> 3.0"
-    }        
-  }
-  cloud {
-    organization = "my-org"
-    workspaces {
-      tags = ["networking"]
-    }
-  }
-}
 
 
 # Provider Block
